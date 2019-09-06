1893b843320a175b94bf2cfdeca7e18a57185a74
Emad <emal02002@utb.vaxjo.se>
2019-09-06 09:29:15 +0200

    La till rad

---
+++ Inlämningsuppgift 1/Inlämningsuppgift 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Inlämningsuppgift_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello ");
+            Console.WriteLine("Hej");
+
+
+        }
+    }
+}
