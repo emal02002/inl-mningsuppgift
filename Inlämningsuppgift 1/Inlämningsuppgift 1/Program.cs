42052d65b1a3d160328ff1f9befdd8cb5f9b3b41
Emad <emal02002@utb.vaxjo.se>
2019-09-06 09:53:55 +0200

    fortsatt med uppgiften

--- Inlämningsuppgift 1/Inlämningsuppgift 1/Program.cs
+++ Inlämningsuppgift 1/Inlämningsuppgift 1/Program.cs
@@ -6,10 +6,19 @@ namespace Inlämningsuppgift_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Skriv ett tal mellan 1 och 100 ");
-            int tal = int.Parse(Console.ReadLine());
+            Random genertator = new Random();
+            int randomTal = genertator.Next(1, 100);
+            while(Tal != randomTal)
+            {
+                Console.WriteLine("Skriv ett tal mellan 1 och 100 ");
+                int tal = int.Parse(Console.ReadLine());
+                if(tal > randomTal)
+                    Console.WriteLine("Det gissade talet är")
 
-            
+
+
+            }
+
 
 
         }
