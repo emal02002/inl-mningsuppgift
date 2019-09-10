f4575f06b984247792ffe5ff0b5cbc62f595a224
Emad <emal02002@utb.vaxjo.se>
2019-09-10 13:43:58 +0200

    har gjort ett gissning spel

--- Inlämningsuppgift 1/Inlämningsuppgift 1/Program.cs
+++ Inlämningsuppgift 1/Inlämningsuppgift 1/Program.cs
@@ -7,20 +7,25 @@ namespace Inlämningsuppgift_1
         static void Main(string[] args)
         {
             Random genertator = new Random();
-            int randomTal = genertator.Next(1, 100);
-            while(Tal != randomTal)
+            int randomTal = genertator.Next(1, 100), gissningar=0;
+            int tal = 0;
+            while(tal != randomTal)
             {
                 Console.WriteLine("Skriv ett tal mellan 1 och 100 ");
-                int tal = int.Parse(Console.ReadLine());
-                if(tal > randomTal)
-                    Console.WriteLine("Det gissade talet är")
+                tal = Convert.ToInt32(Console.ReadLine());
+
+                if (tal > randomTal)
+                    Console.WriteLine("Gissa ett lägre tal");
+                else if (tal < randomTal)
+                    Console.WriteLine("Gissa ett högre tal");
 
 
-
+                gissningar++;
             }
-
-
+                Console.WriteLine("Du har gissat rätt");
+                Console.WriteLine("Du gissade" + gissningar + "gånger");
+            }
 
         }
     }
-}
+
